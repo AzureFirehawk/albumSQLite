7e8265b5608fd2c7647255d8cc460574e5e958c4
AzureFirehawk <madison.thomas094@gmail.com>
2026-01-30 10:51:02 -0600

    function stubs

--- albums.py
+++ albums.py
@@ -3,3 +3,26 @@ Albums Module:
 CRUD functions for albums
 """
 
+def add_album(conn):
+  """
+  Add new album to database
+  """
+  pass
+
+def edit_album(conn):
+  """
+  Edit album in database
+  """
+  pass
+
+def delete_album(conn):
+  """
+  Delete album from database
+  """
+  pass
+
+def view_by_artist(conn, artist_id):
+  """
+  View albums by artist
+  """
+  pass
--- stats.py
+++ stats.py
@@ -2,3 +2,11 @@
 Stats Module:
 Read-only aggregation functions from database
 """
+
+from utils import get_menu_choice
+
+def show_stats_menu(conn):
+  """
+  Show stats menu and prompt user for choice
+  """
+  pass
