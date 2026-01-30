62e564fd1d369792a7ae5fbba0744279b9e39476
AzureFirehawk <madison.thomas094@gmail.com>
2026-01-30 10:50:45 -0600

    update structure to helper functions

--- connect.py
+++ connect.py
@@ -1,22 +1,25 @@
 import sqlite3
 
+def connection():
+  return sqlite3.connect('albums.db')
+
 conn = sqlite3.connect('albums.db')
 
-c = conn.cursor()
+def initialize(conn):
+  c = conn.cursor()
 
-c.execute("""CREATE TABLE artists (
-  artist_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT UNIQUE NOT NULL
-)""")
+  c.execute("""CREATE TABLE IF NOT EXISTS artists (
+    artist_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL
+  )""")
 
-c.execute("""CREATE TABLE albums (
-  album_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  artist_id INTEGER NOT NULL,
-  title TEXT NOT NULL,
-  genre TEXT,
-  FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
-          ON DELETE CASCADE
-)""")
+  c.execute("""CREATE TABLE IF NOT EXISTS albums (
+    album_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    artist_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    genre TEXT,
+    FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
+            ON DELETE CASCADE
+  )""")
 
-conn.commit()
-conn.close()
+  conn.commit()
