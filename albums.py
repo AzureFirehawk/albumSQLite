1a90cf30e660341233adb9e4150669d8e27667d1
AzureFirehawk <madison.thomas094@gmail.com>
2026-01-31 11:37:58 -0600

    add artist implimented

--- albums.py
+++ albums.py
@@ -3,11 +3,19 @@ Albums Module:
 CRUD functions for albums
 """
 
+from artists import show_or_add_artist
+from utils import get_menu_choice
+
 def add_album(conn):
   """
   Add new album to database
   """
-  pass
+  print("\n== Add Album ==")
+  artist_id, name = show_or_add_artist(conn)
+  title = prompt_album_title()
+  genre = prompt_album_genre()
+  insert_album(conn, artist_id, title, genre)
+  print(f"{title} by {name} has been added to the database.")
 
 def edit_album(conn):
   """
@@ -26,3 +34,38 @@ def view_by_artist(conn, artist_id):
   View albums by artist
   """
   pass
+
+def display_albums(albums):
+  """
+  Display list of albums
+  """
+  pass
+
+# user input prompts
+def prompt_album_title():
+  """
+  Prompt user for album title
+  """
+  while True:
+    title = input("Enter the album title: ").strip()
+    if title:
+      return title.title()
+
+    print("Album title cannot be blank.")
+
+def prompt_album_genre():
+  """
+  Prompt user for album genre
+  """
+  genre = input("Enter the album genre: ").strip()
+  return genre.title() if genre else None
+
+# SQL helpers
+def insert_album(conn, artist_id, title, genre=None):
+  c = conn.cursor()
+  c.execute("""
+    INSERT INTO albums (artist_id, title, genre)
+    VALUES (?, ?, ?)
+  """, (artist_id, title, genre)
+  )
+  conn.commit()
